ff974a178d78121490723fdc3cea24f54e199019
FikaKarin <mailtolundqvist@gmail.com>
2022-10-06 16:58:13 +0200

    json object, useState in Welcome comp

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -13,34 +13,30 @@ const Navbar = () => {
   const navigate = useNavigate();
 
   return (
-    <div>
-      <div className='navbar'>
-        <Link onClick={() => navigate(-1)}>
-          <HiArrowLeft className='link-left' />
-        </Link>
-        <Link to='/Welcome'>
-          <GrHomeRounded className='link-center' />
-        </Link>
-        <Popup
-          trigger={<button className='infoButton'> <GrStatusInfo /> </button>}
-          modal
-          nested
-        >
-          {(close) => (
-            <div className='modal'>
-              <button className='close' onClick={close}>
-                &times;
-              </button>
-              <div className='header'> INFO </div>
-              <div className='content'>
-                {' '}
-                På den här sidan ska du:
-              </div>
-            </div>
-          )}
-        </Popup>
-      </div>
-    </div>
+    <div className="navbarContent">
+            <button id="navbarBtn" onClick={() => navigate(-1)}>< HiArrowLeft /></button>
+            <Link to="/Welcome" id="homeLink" ><GrHomeRounded /></Link>
+            <Popup
+                trigger={<button className="button" id="navbarInfo"> < GrStatusInfo /> </button>}
+                modal
+                nested
+            >
+                {close => (
+                    < div className="modal">
+                        <button className="close" onClick={close}>
+                            &times;
+                        </button>
+                        <div className="content">
+                            *Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptas consequuntur doloribus eveniet. Iste laborum quos earum harum, dignissimos modi obcaecati? Magnam molestiae modi non recusandae odit sed veritatis mollitia dolorum!
+                            <br />
+                        </div>
+
+                    </div>
+                )
+                }
+            </Popup >
+
+        </div >
   );
 };
 
--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,16 +1,53 @@
 //välkomsttext
-import { Link } from "react-router-dom";
-import "../style/Welcome.css";
+import { Link } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import '../style/Welcome.css';
 import Navbar from './Navbar';
-
+import '../style/Button.css';
 
 const Welcome = () => {
-    return ( <div>
-        <Navbar />
-        <p>VÄLKOMMEN ANNA</p>
-        <Link to="/Select">Till VÄLJ PAKET</Link>
+  const [data, setData] = useState([]);
+  const getData = () => {
+    fetch('db.json', {
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+      },
+    })
+      .then(function (response) {
+        console.log(response);
+        return response.json();
+      })
+      .then(function (myJson) {
+        console.log(myJson);
+        setData(myJson);
+      });
+  };
+  useEffect(() => {
+    getData();
+  }, []);
 
-    </div> );
-}
- 
+  return (
+    <div>
+      <Navbar />
+      <div className='welcome-content'>
+        <div className='welcome-header'>
+          <h1>Välkommen</h1>
+          <div>
+            {data && data.length > 0 && data.map((item) => 
+            <p>{item.fName} </p>
+            )}
+            </div>
+            <p>, du har</p>
+        
+        </div>
+        <div className='welcome-box'>BOX</div>
+        <button className='btn'>
+          <Link to='/Select'>Gå vidare</Link>
+        </button>
+      </div>
+    </div>
+  );
+};
+
 export default Welcome;
